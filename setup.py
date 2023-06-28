b61f4b0b4669e4e6157272b4cb294d274decae1a
htjb <harrytjbevins@yahoo.co.uk>
2023-06-28 13:03:29 +0100

    bumping minor version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme(short=False):
 
 setup(
     name='margarine',
-    version='0.5.0',
+    version='0.5.1',
     description='margarine: Posterior Sampling and Marginal Bayesian Statistics',
     long_description=readme(),
     author='Harry T. J. Bevins',
