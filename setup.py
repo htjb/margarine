45425845fca61e394fcf51b4bbbe0241653a405f
htjb <harrytjbevins@yahoo.co.uk>
2023-08-30 16:11:43 +0100

    bumping version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme(short=False):
 
 setup(
     name='margarine',
-    version='1.1.1',
+    version='1.1.2',
     description='margarine: Posterior Sampling and Marginal Bayesian Statistics',
     long_description=readme(),
     author='Harry T. J. Bevins',
