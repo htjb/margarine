60fe6b0f6421b8ba928d04737c1f5b791b9695bb
yallup <david.yallup@gmail.com>
2023-11-10 11:26:13 +0000

    increment version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme(short=False):
 
 setup(
     name='margarine',
-    version='1.1.4',
+    version='1.2.0',
     description='margarine: Posterior Sampling and Marginal Bayesian Statistics',
     long_description=readme(),
     author='Harry T. J. Bevins',
