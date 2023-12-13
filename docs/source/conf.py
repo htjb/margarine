d17a055e20e30406c2bddc86aaf46103c8a9db8f
lukashergt <lthergt@phas.ubc.ca>
2023-12-13 14:48:08 -0800

    add autosummary to docs extensions, exclude class members toctree

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ release = get_version()
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
@@ -63,12 +64,16 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 source_suffix = '.rst'
-master_doc='index'
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
+
+autosummary_generate = True
+
+numpydoc_class_members_toctree = False
 
 
 # -- Options for HTML output -------------------------------------------------
