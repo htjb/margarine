da3b55aba9d9c70f56d7eeea7cced65e52ecfa8b
htjb <harrytjbevins@yahoo.co.uk>
2025-11-27 12:59:08 +0000

    adding in a log prob function with correction

--- margarine/estimators/kde.py
+++ margarine/estimators/kde.py
@@ -2,9 +2,17 @@
 
 import jax.numpy as jnp
 import jax.scipy.stats as stats
+from tensorflow_probability.substrates import jax as tfp
 
 from margarine.base.baseflow import BaseDensityEstimator
-from margarine.utils import approximate_bounds
+from margarine.utils import (
+    approximate_bounds,
+    forward_transform,
+    inverse_transform,
+)
+
+tfb = tfp.bijectors
+tfd = tfp.distributions
 
 
 class KDE(BaseDensityEstimator):
@@ -31,7 +39,7 @@ class KDE(BaseDensityEstimator):
         self.bandwidth = bandwidth
 
         if self.weights is None:
-            self.weights = jnp.ones_like(self.theta)
+            self.weights = jnp.ones(len(self.theta))
 
         if theta_ranges is None:
             self.theta_ranges = jnp.concatenate(
@@ -40,4 +48,61 @@ class KDE(BaseDensityEstimator):
 
     def train(self) -> stats.gaussian_kde:
         """Generates a weighted KDE."""
-        return None
+        phi = forward_transform(
+            self.theta, self.theta_ranges[1], self.theta_ranges[0]
+        )
+        weights = self.weights / jnp.sum(self.weights)
+        print(weights)
+        self.kde = stats.gaussian_kde(
+            phi.T, weights=weights, bw_method=self.bandwidth
+        )
+        return self.kde
+
+    def sample(self, key: jnp.ndarray, num_samples: int) -> jnp.ndarray:
+        """Sample from the KDE.
+
+        Args:
+            key: JAX random key for sampling.
+            num_samples: Number of samples to draw.
+
+        Returns:
+            jnp.ndarray: Samples drawn from the KDE.
+        """
+        x = self.kde.resample(key, (num_samples,)).T
+        x = inverse_transform(x, self.theta_ranges[1], self.theta_ranges[0])
+        return x
+
+    def log_prob(self, x: jnp.ndarray) -> jnp.ndarray:
+        """Compute the log-probability of given samples.
+
+        While the density estimator has its own built in log probability
+        function, a correction has to be applied for the transformation of
+        variables that is used to improve accuracy when learning. The
+        correction is implemented here.
+
+        Args:
+            x: Samples for which to compute the log-probability.
+
+        Returns:
+            jnp.ndarray: Log-probabilities of the samples.
+        """
+        transformed_x = forward_transform(
+            x, self.theta_ranges[1], self.theta_ranges[0]
+        )
+
+        transform_chain = tfb.Chain(
+            [
+                tfb.Invert(tfb.NormalCDF()),
+                tfb.Scale(1 / (self.theta_ranges[1] - self.theta_ranges[0])),
+                tfb.Shift(-self.theta_ranges[0]),
+            ]
+        )
+
+        def norm_jac(y: jnp.ndarray) -> jnp.ndarray:
+            """Calculate the normalising jacobian for the transformation."""
+            return transform_chain.inverse_log_det_jacobian(y, event_ndims=0)
+
+        correction = norm_jac(x).sum(axis=1)
+
+        log_probs = jnp.log(self.kde.pdf(transformed_x.T)) + correction
+        return log_probs
