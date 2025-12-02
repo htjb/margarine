c5c7b48263c38312a12454185db741b5d339473e
htjb <harrytjbevins@yahoo.co.uk>
2025-12-02 13:46:03 +0000

    modifying the clustered_distribution example

--- tests/clustered_distributions.py
+++ tests/clustered_distributions.py
@@ -4,85 +4,79 @@ Code modified from
 https://github.com/VincentStimper/normalizing-flows repository.
 """
 
-import numpy as np
-import torch
-from torch import nn
+import jax
+import jax.numpy as jnp
 
 
-class Target(nn.Module):
+class Target:
     """Sample target distributions to test models."""
 
     def __init__(
         self,
-        prop_scale: torch.Tensor = torch.tensor(6.0),
-        prop_shift: torch.Tensor = torch.tensor(-3.0),
+        prop_scale: int | float = 6.0,
+        prop_shift: int | float = -3.0,
     ) -> None:
         """Constructor.
 
         Args:
-          prop_scale (tf.Tensor): Scale for the uniform proposal
-          prop_shift (tf.Tensor): Shift for the uniform proposal
+          prop_scale (jnp.ndarray): Scale for the uniform proposal
+          prop_shift (jnp.ndarray): Shift for the uniform proposal
         """
         super().__init__()
-        self.register_buffer("prop_scale", prop_scale)
-        self.register_buffer("prop_shift", prop_shift)
+        self.prop_scale = jnp.array(prop_scale)
+        self.prop_shift = jnp.array(prop_shift)
+        self.n_dims = len(self.prop_scale)
 
-    def log_prob(self, z: torch.Tensor) -> torch.Tensor:
+    def log_prob(self, z: jnp.ndarray) -> jnp.ndarray:
         """Base log probability function.
 
         Args:
-          z: value or batch of latent variable
+          z (jnp.ndarray): value or batch of latent variable
 
         Returns:
-          log probability of the distribution for z
+          jnp.ndarray: log probability of the distribution for z
         """
         raise NotImplementedError(
             "The log probability is not implemented yet."
         )
 
-    def rejection_sampling(self, num_steps: int = 1) -> torch.Tensor:
-        """Perform rejection sampling on image distribution.
+    def rejection_sampling(
+        self, key: jnp.ndarray, num_steps: int = 1
+    ) -> jnp.ndarray:
+        """Perform rejection sampling on distribution.
 
         Args:
-          num_steps: Number of rejection sampling steps to perform
+          key (jnp.ndarray): JAX random key
+          num_steps (int): Number of rejection sampling steps to perform
 
         Returns:
-          Accepted samples
+          jnp.ndarray: Accepted samples
         """
-        eps = torch.rand(
-            (num_steps, self.n_dims),
-            dtype=self.prop_scale.dtype,
-            device=self.prop_scale.device,
-        )
+        key, subkey1, subkey2 = jax.random.split(key, 3)
+        eps = jax.random.normal(key=subkey1, shape=(num_steps, self.n_dims))
         z_ = self.prop_scale * eps + self.prop_shift
-        prob = torch.rand(
-            num_steps,
-            dtype=self.prop_scale.dtype,
-            device=self.prop_scale.device,
-        )
-        prob_ = torch.exp(self.log_prob(z_) - self.max_log_prob)
+        prob = jax.random.normal(key=subkey2, shape=(num_steps,))
+        prob_ = jnp.exp(self.log_prob(z_) - self.max_log_prob)
         accept = prob_ > prob
         z = z_[accept, :]
         return z
 
-    def sample(self, num_samples: int = 1) -> torch.Tensor:
+    def sample(self, key: jnp.ndarray, num_samples: int = 1) -> jnp.ndarray:
         """Sample from image distribution through rejection sampling.
 
         Args:
+          key: JAX random key
           num_samples: Number of samples to draw
 
         Returns:
           Samples
         """
-        z = torch.zeros(
-            (0, self.n_dims),
-            dtype=self.prop_scale.dtype,
-            device=self.prop_scale.device,
-        )
+        z = jnp.zeros((0, self.n_dims))
         while len(z) < num_samples:
-            z_ = self.rejection_sampling(num_samples)
-            ind = np.min([len(z_), num_samples - len(z)])
-            z = torch.cat([z, z_[:ind, :]], 0)
+            key, subkey = jax.random.split(key)
+            z_ = self.rejection_sampling(subkey, num_samples)
+            ind = jnp.min(jnp.array([len(z_), num_samples - len(z)]))
+            z = jnp.concat([z, z_[:ind, :]], axis=0)
         return z
 
 
@@ -95,7 +89,7 @@ class TwoMoons(Target):
         self.n_dims = 2
         self.max_log_prob = 0.0
 
-    def log_prob(self, z: torch.Tensor) -> torch.Tensor:
+    def log_prob(self, z: jnp.ndarray) -> jnp.ndarray:
         """Log probability function for the two moons distribution.
 
         ```
@@ -110,10 +104,15 @@ class TwoMoons(Target):
         Returns:
           log probability of the distribution for z
         """
-        a = torch.abs(z[:, 0])
-        log_prob = (
+        a = jnp.abs(z[:, 0])
+        """log_prob = (
             -0.5 * ((torch.norm(z, dim=1) - 2) / 0.2) ** 2
             - 0.5 * ((a - 2) / 0.3) ** 2
             + torch.log(1 + torch.exp(-4 * a / 0.09))
+        )"""
+        log_prob = (
+            -0.5 * ((jnp.linalg.norm(z, axis=1) - 2) / 0.2) ** 2
+            - 0.5 * ((a - 2) / 0.3) ** 2
+            + jnp.log(1 + jnp.exp(-4 * a / 0.09))
         )
         return log_prob
