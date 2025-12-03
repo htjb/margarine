a1512c3eb09eecb9188b86832bab7ab087cc3f91
htjb <harrytjbevins@yahoo.co.uk>
2025-12-03 16:24:27 +0000

    playing with the tests. most pass now just trouble with maf training on toy example

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -39,7 +39,7 @@ def d_g(logPpi: jnp.ndarray, weights: jnp.ndarray) -> jnp.ndarray:
     return 2 * jnp.cov(logPpi, aweights=weights)
 
 
-nsamples = 2500
+nsamples = 5000
 key = jax.random.PRNGKey(0)
 
 original_samples = jax.random.multivariate_normal(
@@ -78,7 +78,7 @@ def test_nice() -> None:
         original_samples,
         weights=weights,
         in_size=2,
-        hidden_size=128,
+        hidden_size=50,
         num_layers=2,
         num_coupling_layers=4,
     )
@@ -91,27 +91,54 @@ def test_nice() -> None:
     error = jnp.mean(jnp.abs(inverse_transformed - z))
     assert error < 1e-3
 
-    key, subkey = jax.random.split(key)
-    nice_estimator.train(subkey, learning_rate=1e-3, epochs=1000, patience=50)
+    kld_estimates, bmd_estimates = [], []
+    for _ in range(5):
+        nice_estimator = NICE(
+            original_samples,
+            weights=weights,
+            in_size=2,
+            hidden_size=50,
+            num_layers=2,
+            num_coupling_layers=6,
+            theta_ranges=bounds,
+        )
 
-    key, subkey = jax.random.split(key)
-    samples = nice_estimator.sample(subkey, 10000)
+        key, subkey = jax.random.split(key)
+        nice_estimator.train(subkey, learning_rate=1e-3, epochs=2000, patience=50)
 
-    prior_estimator = NICE(
-        prior_samples,
-        in_size=2,
-        hidden_size=128,
-        num_layers=2,
-        num_coupling_layers=4,
-    )
-    prior_estimator.train(subkey, learning_rate=1e-3, epochs=1000, patience=50)
+        key, subkey = jax.random.split(key)
+        samples = nice_estimator.sample(subkey, 5000)
 
-    # check the kl divergence and model dimensionality
-    kld = kldivergence(nice_estimator, prior_estimator, samples)
-    dim = model_dimensionality(nice_estimator, prior_estimator, samples)
+        prior_estimator = NICE(
+            prior_samples,
+            in_size=2,
+            hidden_size=50,
+            num_layers=2,
+            num_coupling_layers=6,
+            theta_ranges=bounds,
+        )
+        key, subkey = jax.random.split(key)
+        prior_estimator.train(subkey, learning_rate=1e-3, epochs=2000, patience=50)
 
-    assert_allclose(kld, samples_kl, rtol=0.5, atol=0.5)
-    assert_allclose(dim, samples_d, rtol=0.5, atol=0.5)
+        # check the kl divergence and model dimensionality
+        kld = kldivergence(nice_estimator, prior_estimator, samples)
+        dim = model_dimensionality(nice_estimator, prior_estimator, samples)
+        kld_estimates.append(kld)
+        bmd_estimates.append(dim)
+
+    kld_estimates = jnp.array(kld_estimates)
+    bmd_estimates = jnp.array(bmd_estimates)
+
+    kl_rtol = 3*jnp.std(kld_estimates) / (jnp.mean(kld_estimates) + 1e-10)
+    kl_atol = 3*jnp.std(kld_estimates)
+    kld = jnp.mean(kld_estimates)
+
+    dim_rtol = 3*jnp.std(bmd_estimates) / (jnp.mean(bmd_estimates) + 1e-10)
+    dim_atol = 3*jnp.std(bmd_estimates)
+    dim = jnp.mean(bmd_estimates)
+
+    assert_allclose(kld, samples_kl, rtol=kl_rtol, atol=kl_atol)
+    assert_allclose(dim, samples_d, rtol=dim_rtol, atol=dim_atol)
 
 
 def test_realnvp() -> None:
@@ -122,8 +149,8 @@ def test_realnvp() -> None:
         original_samples,
         weights=weights,
         in_size=2,
-        hidden_size=128,
-        num_coupling_layers=6,
+        hidden_size=50,
+        num_coupling_layers=2,
     )
 
     # check the forward and inverse transformations
@@ -135,11 +162,6 @@ def test_realnvp() -> None:
     error = jnp.mean(jnp.abs(inverse_transformed - z))
     assert error < 1e-3
 
-    key, subkey = jax.random.split(key)
-    realnvp_estimator.train(
-        subkey, learning_rate=1e-3, epochs=1000, patience=50
-    )
-
     kl_estimates, bmd_estimates = [], []
     for _ in range(5):
         realnvp_estimator = RealNVP(
@@ -147,22 +169,24 @@ def test_realnvp() -> None:
             weights=weights,
             in_size=2,
             hidden_size=50,
-            num_coupling_layers=3,
+            num_coupling_layers=2,
         )
         key, subkey = jax.random.split(key)
         realnvp_estimator.train(
-            subkey, learning_rate=1e-4, epochs=1000, patience=50
+            subkey, learning_rate=1e-4, epochs=2000, patience=50
         )
 
         key, subkey = jax.random.split(key)
-        samples = realnvp_estimator.sample(subkey, 10000)
+        samples = realnvp_estimator.sample(subkey, 5000)
 
         prior_estimator = RealNVP(
-            prior_samples, in_size=2, hidden_size=50, num_coupling_layers=3
+            prior_samples, in_size=2, hidden_size=50, num_coupling_layers=2
         )
+        key, subkey = jax.random.split(key)
         prior_estimator.train(
-            subkey, learning_rate=1e-4, epochs=1000, patience=50
+            subkey, learning_rate=1e-4, epochs=2000, patience=50
         )
+
         # check the kl divergence and model dimensionality
         kld = kldivergence(realnvp_estimator, prior_estimator, samples)
         dim = model_dimensionality(realnvp_estimator, prior_estimator, samples)
@@ -170,39 +194,51 @@ def test_realnvp() -> None:
         bmd_estimates.append(dim)
 
     kl_estimates = jnp.array(kl_estimates)
-    dim = jnp.mean(jnp.array(bmd_estimates))
-    kl_rtol = jnp.std(kl_estimates) / (jnp.mean(kl_estimates) + 1e-10)
-    kl_atol = jnp.std(kl_estimates)
+    bmd_estimates = jnp.array(bmd_estimates)
+
+    kl_rtol = 3*jnp.std(kl_estimates) / (jnp.mean(kl_estimates) + 1e-10)
+    kl_atol = 3*jnp.std(kl_estimates)
     kld = jnp.mean(kl_estimates)
-    print(kld, dim)
-    print(samples_kl, samples_d)
-    print("KL rtol:", kl_rtol, "KL atol:", kl_atol)
+
+    dim_rtol = 3*jnp.std(bmd_estimates) / (jnp.mean(bmd_estimates) + 1e-10)
+    dim_atol = 3*jnp.std(bmd_estimates)
+    dim = jnp.mean(bmd_estimates)
     assert_allclose(kld, samples_kl, rtol=kl_rtol, atol=kl_atol)
-
-    assert_allclose(dim, samples_d, rtol=0.5, atol=0.5)
-
-
-test_realnvp()
+    assert_allclose(dim, samples_d, rtol=dim_rtol, atol=dim_atol)
 
 
 def test_kde() -> None:
     """Test KDE estimator."""
     key = jax.random.PRNGKey(44)
-    kde_estimator = KDE(original_samples, weights=weights, bandwidth=0.2)
-    kde_estimator.train()
-    key, subkey = jax.random.split(key)
-    samples = kde_estimator.sample(subkey, 10000)
+    kl_estimates, bmd_estimates = [], []
+    for _ in range(5):
+        kde_estimator = KDE(original_samples, weights=weights,
+                            theta_ranges=bounds, bandwidth=0.08)
+        kde_estimator.train()
+        key, subkey = jax.random.split(key)
+        samples = kde_estimator.sample(subkey, 5000)
 
-    prior_estimator = KDE(prior_samples, bandwidth=0.2)
-    prior_estimator.train()
+        prior_estimator = KDE(prior_samples, theta_ranges=bounds, bandwidth=0.08)
+        prior_estimator.train()
 
-    # check the kl divergence and model dimensionality
-    kld = kldivergence(kde_estimator, prior_estimator, samples)
-    dim = model_dimensionality(kde_estimator, prior_estimator, samples)
-    print(kld, dim)
-    print(samples_kl, samples_d)
-    assert_allclose(kld, samples_kl, rtol=0.5, atol=0.5)
-    assert_allclose(dim, samples_d, rtol=0.5, atol=0.5)
+        kld = kldivergence(kde_estimator, prior_estimator, samples)
+        dim = model_dimensionality(kde_estimator, prior_estimator, samples)
+        kl_estimates.append(kld)
+        bmd_estimates.append(dim)
+
+    kl_estimates = jnp.array(kl_estimates)
+    bmd_estimates = jnp.array(bmd_estimates)
+
+    kl_rtol = 3*jnp.std(kl_estimates) / (jnp.mean(kl_estimates) + 1e-10)
+    kl_atol = 3*jnp.std(kl_estimates)
+    kld = jnp.mean(kl_estimates)
+
+    dim_rtol = 3*jnp.std(bmd_estimates) / (jnp.mean(bmd_estimates) + 1e-10)
+    dim_atol = 3*jnp.std(bmd_estimates)
+    dim = jnp.mean(bmd_estimates)
+
+    assert_allclose(kld, samples_kl, rtol=kl_rtol, atol=kl_atol)
+    assert_allclose(dim, samples_d, rtol=dim_rtol, atol=dim_atol)
 
 
 def test_maf() -> None:
@@ -227,28 +263,53 @@ def test_maf() -> None:
     error = jnp.mean(jnp.abs(inverse_transformed - z))
     assert error < 5e-3  # MAF is less precise here due to numerical issues
 
-    # mafs are really hard to train well - so commenting out for now
-    key, subkey = jax.random.split(key)
-    maf_estimator.train(subkey, learning_rate=1e-3, epochs=1000, patience=50)
+    # mafs are really hard to train well
+    kl_estimates, bmd_estimates = [], []
+    for _ in range(5):
+        maf_estimator = MAF(
+            original_samples,
+            weights=weights,
+            in_size=2,
+            hidden_size=50,
+            num_layers=2,
+            num_made_networks=5,
+            theta_ranges=bounds,
+        )
+        key, subkey = jax.random.split(key)
+        maf_estimator.train(subkey, learning_rate=1e-3, epochs=1000, patience=50)
 
-    key, subkey = jax.random.split(key)
-    samples = maf_estimator.sample(subkey, 10000)
+        key, subkey = jax.random.split(key)
+        samples = maf_estimator.sample(subkey, 5000)
 
-    prior_estimator = MAF(
-        prior_samples,
-        in_size=2,
-        hidden_size=50,
-        num_layers=2,
-        num_made_networks=5,
-        theta_ranges=bounds,
-    )
-    prior_estimator.train(subkey, learning_rate=1e-3, epochs=1000, patience=50)
+        prior_estimator = MAF(
+            prior_samples,
+            in_size=2,
+            hidden_size=50,
+            num_layers=2,
+            num_made_networks=5,
+            theta_ranges=bounds,
+        )
+        key, subkey = jax.random.split(key)
+        prior_estimator.train(subkey, learning_rate=1e-3, epochs=1000, patience=50)
 
-    # check the kl divergence and model dimensionality
-    kld = kldivergence(maf_estimator, prior_estimator, samples)
-    dim = model_dimensionality(maf_estimator, prior_estimator, samples)
-    assert_allclose(kld, samples_kl, rtol=0.5, atol=0.5)
-    assert_allclose(dim, samples_d, rtol=0.5, atol=0.5)
+        # check the kl divergence and model dimensionality
+        kld = kldivergence(maf_estimator, prior_estimator, samples)
+        dim = model_dimensionality(maf_estimator, prior_estimator, samples)
+        kl_estimates.append(kld)
+        bmd_estimates.append(dim)
+
+    kl_estimates = jnp.array(kl_estimates)
+    bmd_estimates = jnp.array(bmd_estimates)
+
+    kl_rtol = 3*jnp.std(kl_estimates) / (jnp.mean(kl_estimates) + 1e-10)
+    kl_atol = 3*jnp.std(kl_estimates)
+    kld = jnp.mean(kl_estimates)
+
+    bmd_rtol = 3*jnp.std(bmd_estimates) / (jnp.mean(bmd_estimates) + 1e-10)
+    bmd_atol = 3*jnp.std(bmd_estimates)
+    dim = jnp.mean(bmd_estimates)
+    assert_allclose(kld, samples_kl, rtol=kl_rtol, atol=kl_atol)
+    assert_allclose(dim, samples_d, rtol=bmd_rtol, atol=bmd_atol)
 
     # check permuatations
     maf_estimator = MAF(
