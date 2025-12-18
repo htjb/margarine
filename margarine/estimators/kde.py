2b21364a4a6da59fe709908f05ff23deba9159dc
htjb <harrytjbevins@yahoo.co.uk>
2025-12-18 10:55:10 +0000

    more consistent save and load functions for kdes

--- margarine/estimators/kde.py
+++ margarine/estimators/kde.py
@@ -1,9 +1,13 @@
 """KDE implementation using JAX."""
 
 import pickle
+import shutil
+from pathlib import Path
+from zipfile import ZipFile
 
 import jax.numpy as jnp
 import jax.scipy.stats as stats
+import yaml
 from tensorflow_probability.substrates import jax as tfp
 
 from margarine.base.baseflow import BaseDensityEstimator
@@ -145,28 +149,79 @@ class KDE(BaseDensityEstimator):
 
         return self.log_prob(x) + logevidence - prior_density
 
-    def save(self, filepath: str) -> None:
+    def save(self, filename: str) -> None:
         """Save the KDE to a file.
 
         Args:
-            filepath: Path to the file where the KDE will be saved.
+            filename (str): Path to the file where the KDE will be saved.
         """
-        with open(filepath, "wb") as f:
+        path = Path(filename).resolve()
+        if path.exists():
+            shutil.rmtree(path)
+
+        with open(f"{path}/kde.pkl", "wb") as f:
             pickle.dump(self.kde, f)
 
+        config = {
+            "bandwidth": self.bandwidth,
+        }
+
+        with open(f"{path}/config.yaml", "w") as f:
+            yaml.dump(config, f)
+
+        metadata = {
+            "theta": self.theta,
+            "weights": self.weights,
+            "theta_ranges": self.theta_ranges,
+        }
+
+        with open(f"{path}/metadata.yaml", "w") as f:
+            yaml.dump(metadata, f)
+
+        with ZipFile(filename + ".marg", "w") as z:
+            for subpath in path.rglob("*"):
+                if subpath.is_file():
+                    z.write(subpath, arcname=subpath.relative_to(path))
+
+        shutil.rmtree(path)
+
     @classmethod
-    def load(cls, filepath: str) -> "KDE":
+    def load(cls, filename: str) -> "KDE":
         """Load a KDE from a file.
 
         Args:
-            filepath: Path to the file from which to load the KDE.
+            filename (str): Path to the file from which to load the KDE.
 
         Returns:
             KDE: Loaded KDE instance.
         """
-        with open(filepath, "rb") as f:
+        zip_path = Path(f"{filename}.marg")
+        path = Path(filename + ".tmp").resolve()
+        with ZipFile(zip_path) as z:
+            # Extract all files to a folder
+            z.extractall(path)
+
+        with open(f"{path}/config.yaml") as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+
+        instance = cls(
+            theta=jnp.array([]),  # Placeholder, will be overwritten
+            weights=jnp.array([]),  # Placeholder, will be overwritten
+            theta_ranges=jnp.array([]),  # Placeholder, will be overwritten
+            bandwidth=config["bandwidth"],
+        )
+
+        with open(f"{path}/metadata.yaml") as f:
+            metadata = yaml.load(f, Loader=yaml.FullLoader)
+
+        instance.theta = jnp.array(metadata["theta"])
+        instance.weights = jnp.array(metadata["weights"])
+        instance.theta_ranges = jnp.array(metadata["theta_ranges"])
+
+        with open(f"{path}/kde.pkl", "rb") as f:
             kde = pickle.load(f)
 
-        instance = cls(jnp.array([]))  # Dummy initialization
         instance.kde = kde
+
+        shutil.rmtree(path)
         return instance
