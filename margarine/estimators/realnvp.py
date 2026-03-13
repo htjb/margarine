f660fb911914760b466cb822ee54660adca91db4
Harry Bevins <40355093+htjb@users.noreply.github.com>
2026-03-13 08:59:22 +0000

    Fixing bug with the way the training and validaiton loss is stored (#73)
    * fixing bug with the way the training and validaiton loss is being stored in the objects
    
    * put the tests on a cron job
    
    * bumping version number;
    
    * ooopa i think i put the schedule in the wrong place

--- margarine/estimators/realnvp.py
+++ margarine/estimators/realnvp.py
@@ -359,8 +359,8 @@ class RealNVP(BaseDensityEstimator, nnx.Module):
                 best_loss=f"{best_loss:.3e}",
             )
 
-        self.train_loss = tl
-        self.val_loss = vl
+        self.train_loss = jnp.array(tl)
+        self.val_loss = jnp.array(vl)
 
         if best_model is not None:
             nnx.update(self, best_model)
